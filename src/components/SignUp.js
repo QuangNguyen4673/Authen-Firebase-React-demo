bf404b82dd7c61a367f23816345d1b43bc503499
Quang Nguyen <quangnguyenmail96@gmail.com>
2021-07-14 19:54:14 +0700

    done project

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,24 +1,43 @@
 import React, { useRef, useState } from "react";
 import { Button, Card, Form, Alert } from "react-bootstrap";
-import { useAuth } from "../AuthProvider";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import * as faker from "faker/locale/en";
 
 const SignUp = () => {
   const userNameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
 
   const [loading, setLoading] = useState(false);
-  const { signup, currentUser } = useAuth();
   const [dumpBtn, setDumpBtn] = useState(false);
+  const history = useHistory();
+  const { signUp, currentUser } = useAuth();
 
   const handleClick = async () => {
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return;
+    }
     setLoading(true);
-    await signup(
+    await signUp(
       userNameRef.current.value,
       emailRef.current.value,
       passwordRef.current.value
     );
     setLoading(false);
+    history.push("/login");
+  };
+
+  const passwordAutoFill = () => {
+    passwordRef.current.value = "123456";
+    passwordConfirmRef.current.value = "123456";
+  };
+  const wholeFormAutoFill = () => {
+    userNameRef.current.value = faker.name.findName();
+    emailRef.current.value = faker.internet.email();
+    passwordRef.current.value = "123456";
+    passwordConfirmRef.current.value = "123456";
   };
 
   return (
@@ -26,10 +45,16 @@ const SignUp = () => {
       <button type="button" onClick={() => setDumpBtn(!dumpBtn)}>
         Click to re-render
       </button>
+      <button type="button" onClick={passwordAutoFill}>
+        Fill password
+      </button>
+      <button type="button" onClick={wholeFormAutoFill}>
+        Fill all
+      </button>
       <Card className="w-100 mx-auto mt-5" style={{ maxWidth: "400px" }}>
         <Card.Body>
           <h2 className="text-center mb-4">Sign up</h2>
-          {currentUser.email}
+          {currentUser && currentUser.email}
           <Form>
             <Form.Group id="username">
               <Form.Label>Your Name</Form.Label>
@@ -43,6 +68,10 @@ const SignUp = () => {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} />
             </Form.Group>
+            <Form.Group id="passwordConfirm">
+              <Form.Label>Password Confirm</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} />
+            </Form.Group>
             <Button
               disabled={loading}
               onClick={handleClick}
@@ -55,7 +84,7 @@ const SignUp = () => {
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? Log In
+        Already have an account? <Link to="/login">Log In</Link>
       </div>
     </>
   );
